b9febd4dedbb87ee241b2631b304bf6fd3913ce6
parth Jansari <parthjansari@outlook.com>
2016-09-25 10:14:36 +0530

    Add files via upload

--- _assets/_javascripts/app.js
+++ _assets/_javascripts/app.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+$(function(){
+  $(".links i.fa,.links .dropdown").hover(function() {
+    $(".links .dropdown").css("display","block")
+  }, function() {
+    $(".links .dropdown").css("display","none")
+  });
+  $(".links i.fa").click(function(event) {
+    /* Act on the event */  $(".links .dropdown").css("display","block")
+  });
+  $(".links ul li").click(function(){
+     $(".links .dropdown").css("display","none")
+  })
+})
